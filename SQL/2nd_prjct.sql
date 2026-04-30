8f04fcacea5a8eaa6e57ce890adafc1e2740c32f
Ankita Das <adas96714@gmail.com>
2026-04-30 10:59:36 +0530

    Create 2nd_prjct.sql
    -- Summary of project 1,2
    
    -- Database create, use, table create, insert, retrieve, delete table, delete database
    -- Clause: WHERE
    -- Constraints: UNIQUE, ORDER BY, DISTINCT, DEFAULT, PRIMARY KEY, NOT NULL, AUTO_INCREMENT
    -- LIKE (S%, A%, %i%), BETWEEN, IN('BCA', 'MSC'), AND, OR

---
+++ SQL/2nd_prjct.sql
@@ -0,0 +1,35 @@
+CREATE  DATABASE school_db2;
+USE school_db2;
+CREATE TABLE teachers (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR (30) NOT NULL,
+email VARCHAR(50) UNIQUE,
+subject VARCHAR(30) DEFAULT 'Math',
+salary  INT);
+INSERT INTO teachers (name, email, subject, salary)
+VALUES
+('Sahil', 'sahil123@gmail.com', 'English', 20000),
+('Ribani', 'ribani123@gmail.com', 'Math', 25000),
+('Ankita', 'ankita123@gmail.com', 'Computer Science', 22000),
+('Arushi', 'arushi123@gmail.com', 'English', 20000),
+('Riya', 'riya123@gmail.com', DEFAULT, 25000);
+
+SELECT * FROM teachers;
+
+SELECT DISTINCT subject
+ FROM teachers;
+ 
+ SELECT * FROM teachers
+ ORDER BY salary DESC;
+ 
+ SELECT * FROM teachers
+ LIMIT 3;
+ 
+ SELECT *FROM teachers
+ WHERE name LIKE 'R%';
+ 
+ SELECT * FROM teachers
+ WHERE salary BETWEEN 21000 AND 23000;
+ 
+ SELECT * FROM teachers
+ WHERE subject IN ('Math', 'English');
